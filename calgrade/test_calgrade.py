303be232327c5895790e0b2f400162c800e67238
Maxxza1 <635159090014@dpu.ac.th>
2020-12-13 16:13:06 +0700

    Update

---
+++ calgrade/test_calgrade.py
@@ -0,0 +1,19 @@
+from calgrade import calGrade
+import unittest
+
+
+class Test_Calculator(unittest.TestCase):
+    def testSumFunction(self):
+        self.assertEqual(calGrade(70),"B") 
+
+    def testMinusFunction(self):
+        self.assertEqual(calGrade(80),"A") 
+
+    def testMultiplyFunction(self):
+        self.assertEqual(calGrade(1),"F") 
+
+    def testDivideFunction(self):
+        self.assertEqual(calGrade(50), "D")
+
+    def testitFunction(self):
+        self.assertEqual(calGrade(60), "C") 
